b511ab3b4c007a2a90918f5230336f69c5e110d1
Ken <3408238124@qq.com>
2023-06-15 18:32:36 +0800

    second

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # vue3geo README
 
+
+### 1.0.0
+
+Initial release of ...
+
+### 1.0.1
+
+Fixed issue #.
+
+### 1.1.0
+
+test
+
+
+
+
 **Enjoy!**
--- src/extension.ts
+++ src/extension.ts
@@ -3,7 +3,12 @@ import * as vscode from 'vscode';
 export function activate(context: vscode.ExtensionContext) {
     context.subscriptions.push(
         vscode.commands.registerTextEditorCommand('vue3geo.generateVue3Component', (editor: vscode.TextEditor) => {
-            editor.insertSnippet(new vscode.SnippetString(`veo`), editor.selection.active);
+            editor.insertSnippet(new vscode.SnippetString(`veo3`), editor.selection.active);
+        })
+    );
+    context.subscriptions.push(
+        vscode.commands.registerTextEditorCommand('vue3geo.generateVue2Component', (editor: vscode.TextEditor) => {
+            editor.insertSnippet(new vscode.SnippetString(`veo2`), editor.selection.active);
         })
     );
 }
